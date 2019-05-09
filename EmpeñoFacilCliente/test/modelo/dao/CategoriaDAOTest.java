9c3d7d6d630409b1e061630706345dc65ad0abbc
Juan Carlos Domínguez Domínguez <36394805+JuuanC@users.noreply.github.com>
2019-05-09 13:09:09 -0500

    Diseño y pruebas
    Correcciones en el diagarma de casos de uso e implementacion de algunas pruebas

---
+++ EmpeñoFacilCliente/test/modelo/dao/CategoriaDAOTest.java
@@ -0,0 +1,57 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modelo.dao;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Juuan
+ */
+public class CategoriaDAOTest {
+
+    public CategoriaDAOTest() {
+    }
+
+    /*@Test Esta si pasa sin marcar excepciones
+    public void registrarCategoriaExito(){
+        boolean valorEsperado = true;
+        boolean valorObtenido = CategoriaDAO.registrarCategoria(2, "Test");
+        assertEquals("Categoria registrada", valorEsperado, valorObtenido);
+    }*/
+    /*@Test
+    public void registrarCategoriaFracaso() {
+        boolean valorEsperado = false;
+        boolean valorObtenido = CategoriaDAO.registrarCategoria(1, "Test eliminar");
+        assertEquals("Categoria no registrada", valorEsperado, valorObtenido);
+    }*/
+    /*@Test Esta si pasa sin marcar excepciones
+    public void eliminarCategoriaExito(){
+        boolean valorEsperado = true;
+        boolean valorObtenido = CategoriaDAO.eliminarCategoria(34);
+        assertEquals("Categoria eliminada", valorEsperado, valorObtenido);
+    }*/
+ /*@Test Esta si pasa sin marcar excepciones
+    public void eliminarCategoriaFracaso(){
+        boolean valorEsperado = false;
+        boolean valorObtenido = CategoriaDAO.eliminarCategoria(100);
+        assertEquals("Categoria no eliminada", valorEsperado, valorObtenido);
+    }*/
+ /*@Test Esta si pasa sin marcar excepciones
+    public void actualizarCategoriaExito(){
+        boolean valorEsperado = true;
+        boolean valorObtenido = CategoriaDAO.actualizarCategoria(35, "Test actualizar");
+        assertEquals("Categoria no eliminada", valorEsperado, valorObtenido);
+    }*/
+
+ /*@Test Esta si pasa sin marcar excepciones
+    public void actualizarCategoriaFracaso() {
+        boolean valorEsperado = false;
+        boolean valorObtenido = CategoriaDAO.actualizarCategoria(0, "Test actualizar");
+        assertEquals("Categoria no eliminada", valorEsperado, valorObtenido);
+    }*/
+}
---
+++ EmpeñoFacilCliente/test/modelo/dao/UsuarioDAOTest.java
@@ -0,0 +1,59 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modelo.dao;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Juuan
+ */
+public class UsuarioDAOTest {
+
+    public UsuarioDAOTest() {
+    }
+
+    @Test
+    public void resgistrarUsuarioExito() {
+        boolean valorEsperado = true;
+        boolean valorObtenido = UsuarioDAO.registrarUsuario("Test", "Test", "8");
+        assertEquals("Usuario registrado", valorEsperado, valorObtenido);
+    }
+
+    @Test
+    public void resgistrarUsuarioFracaso() {
+        boolean valorEsperado = true;
+        boolean valorObtenido = UsuarioDAO.registrarUsuario("Test", "Test", "8");
+        assertEquals("Usuario no registrado", valorEsperado, valorObtenido);
+    }
+    
+    @Test
+    public void eliminarUsuarioExito(){
+        boolean valorEsperado = true;
+        boolean valorObtenido = UsuarioDAO.eliminarUsuario(8);
+        assertEquals("Usuario eliminado", valorEsperado, valorObtenido);
+    }
+    
+    /*@Test
+    public void eliminarUsuarioFracaso(){
+        boolean valorEsperado = false;
+        boolean valorObtenido = UsuarioDAO.eliminarUsuario(100);
+        assertEquals("Usuario no eliminado", valorEsperado, valorObtenido);
+    }*/
+    
+    public void actualizarUsuarioExito(){
+        boolean valorEsperado = true;
+        boolean valorObtenido = UsuarioDAO.actualizarUsuario();
+        assertEquals("Usuario actualizado", valorEsperado, valorObtenido);
+    }
+    
+    public void actualizarUsuarioFracasp(){
+        boolean valorEsperado = false;
+        boolean valorObtenido = UsuarioDAO.actualizarUsuario();
+        assertEquals("Usuario no actualizado", valorEsperado, valorObtenido);
+    }
+}
