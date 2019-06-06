c997d93dfc9be89d2c270161fd9b18b0e96ee610
Juan Carlos Domínguez Domínguez <36394805+JuuanC@users.noreply.github.com>
2019-06-05 23:30:24 -0500

    Documento

--- EmpeñoFacilCliente/test/modelo/dao/CategoriaDAOTest.java
+++ EmpeñoFacilCliente/test/modelo/dao/CategoriaDAOTest.java
@@ -17,41 +17,51 @@ public class CategoriaDAOTest {
     public CategoriaDAOTest() {
     }
 
-    /*@Test Esta si pasa sin marcar excepciones
-    public void registrarCategoriaExito(){
+    @Test
+    //Esta si pasa sin marcar excepciones
+
+    public void registrarCategoriaExito() {
         boolean valorEsperado = true;
-        boolean valorObtenido = CategoriaDAO.registrarCategoria(2, "Test");
+        boolean valorObtenido = CategoriaDAO.registrarCategoriaPrincipal("Test");
         assertEquals("Categoria registrada", valorEsperado, valorObtenido);
-    }*/
-    /*@Test
+    }
+
+    @Test
     public void registrarCategoriaFracaso() {
         boolean valorEsperado = false;
-        boolean valorObtenido = CategoriaDAO.registrarCategoria(1, "Test eliminar");
+        boolean valorObtenido = CategoriaDAO.registrarCategoriaPrincipal("Test eliminar");
         assertEquals("Categoria no registrada", valorEsperado, valorObtenido);
-    }*/
-    /*@Test Esta si pasa sin marcar excepciones
-    public void eliminarCategoriaExito(){
+    }
+    @Test
+    //Esta si pasa sin marcar excepciones
+
+    public void eliminarCategoriaExito() {
         boolean valorEsperado = true;
         boolean valorObtenido = CategoriaDAO.eliminarCategoria(34);
         assertEquals("Categoria eliminada", valorEsperado, valorObtenido);
-    }*/
- /*@Test Esta si pasa sin marcar excepciones
-    public void eliminarCategoriaFracaso(){
+    }
+    @Test
+    //Esta si pasa sin marcar excepciones
+
+    public void eliminarCategoriaFracaso() {
         boolean valorEsperado = false;
         boolean valorObtenido = CategoriaDAO.eliminarCategoria(100);
         assertEquals("Categoria no eliminada", valorEsperado, valorObtenido);
-    }*/
- /*@Test Esta si pasa sin marcar excepciones
-    public void actualizarCategoriaExito(){
-        boolean valorEsperado = true;
-        boolean valorObtenido = CategoriaDAO.actualizarCategoria(35, "Test actualizar");
-        assertEquals("Categoria no eliminada", valorEsperado, valorObtenido);
-    }*/
+    }
+    @Test
+    //Esta si pasa sin marcar excepciones
 
- /*@Test Esta si pasa sin marcar excepciones
+    public void actualizarCategoriaExito() {
+        boolean valorEsperado = true;
+        boolean valorObtenido = CategoriaDAO.actualizarCategoriaPrincipal(35, "Test actualizar");
+        assertEquals("Categoria no eliminada", valorEsperado, valorObtenido);
+    }
+    @Test
+    //Esta si pasa sin marcar excepciones
+
     public void actualizarCategoriaFracaso() {
         boolean valorEsperado = false;
-        boolean valorObtenido = CategoriaDAO.actualizarCategoria(0, "Test actualizar");
+        boolean valorObtenido = CategoriaDAO.actualizarCategoriaPrincipal(0, "Test actualizar");
         assertEquals("Categoria no eliminada", valorEsperado, valorObtenido);
-    }*/
+    }
 }
